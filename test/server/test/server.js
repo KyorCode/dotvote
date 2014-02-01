68059e329801a8bd1c076f955f52e6ff9291f671
kyorcode <t-c@live.be>
2014-02-01 06:20:15 -0500

    First server test.

--- test/server/test/server.js
+++ test/server/test/server.js
@@ -1,8 +1,11 @@
 var expect = require('chai').expect;
 var request = require('superagent');
 
-describe("obvious()", function() {
-    it("should state", function() {
-        expect(true).to.be.true;
-    })
+describe("Server suite:", function() {
+    it("should be available",function(done){
+       request.get(process.env.IP + ':' + process.env.PORT).end(function(res){
+          expect(res).to.exist;
+          done();
+       });
+    });
 });
